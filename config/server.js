1bf7c6c65ce024e67e34be102dda0c5f391a136f
fabiosdsilva <fabiosds17@gmail.com>
2022-04-27 17:13:38 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '28ac70cdba36afeb38af3ce4bd45e232'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '799b61e0-9361-409b-8072-ed8a8cb9b918'
+};
